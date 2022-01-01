ef1950ff69c23c4f5637961136fc3f1385d1fdf1
Boss9013 <76265722+Boss9013@users.noreply.github.com>
2022-01-02 02:27:41 +0300

    Update plugin

--- src/ru/Boss90/SexSystem/Utils/ColorUtil.java
+++
@@ -1,9 +0,0 @@
-package ru.Boss90.SexSystem.Utils;
-
-import org.bukkit.ChatColor;
-
-public class ColorUtil {
-	public static String parserColor (String text) {
-		return ChatColor.translateAlternateColorCodes('&', text);
-	}
-}
